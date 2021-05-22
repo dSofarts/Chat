7937563a369bc602f1587cdc402bd951e938b6a7
dSofarts <johndoe@example.com>
2021-05-22 18:15:16 +0300

    Response added

--- src/main/java/main/ChatController.java
+++ src/main/java/main/ChatController.java
@@ -1,7 +1,12 @@
 package main;
 
+import main.model.Message;
 import main.model.User;
+import main.repos.MessageRepository;
 import main.repos.UserRepository;
+import main.response.AddMessageResponse;
+import main.response.AuthResponse;
+import main.response.MessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class ChatController {
@@ -18,12 +26,20 @@ public class ChatController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MessageRepository messageRepository;
+
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
+
     @GetMapping(path = "/api/auth")
-    public HashMap<String, Boolean> auth() {
-        HashMap<String, Boolean> response = new HashMap<>();
+    public AuthResponse auth() {
+        AuthResponse response = new AuthResponse();
         String sessionId = getSessionId();
         User user = userRepository.getBySessionId(sessionId);
-        response.put("result", user != null);
+        response.setResult(user != null);
+        if(user != null) {
+            response.setName(user.getName());
+        }
         return response;
     }
 
@@ -38,6 +54,45 @@ public class ChatController {
         userRepository.save(user);
         HashMap<String, Boolean> response = new HashMap<>();
         response.put("result", true);
+        return response;
+    }
+
+    @PostMapping(path = "/api/messages")
+    public AddMessageResponse addMessage(HttpServletRequest request) {
+        String text = request.getParameter("text");
+
+        String sessionId = getSessionId();
+        User user = userRepository.getBySessionId(sessionId);
+
+        Date time = new Date();
+        Message message = new Message();
+        message.setSendTime(time);
+        message.setUser(user);
+        message.setText(text);
+        messageRepository.save(message);
+
+        AddMessageResponse response = new AddMessageResponse();
+        response.setResult(true);
+        response.setTime(formatter.format(time));
+        return response;
+    }
+
+    @GetMapping(path = "/api/messages")
+    public HashMap<String, List> getMessages() {
+        ArrayList<MessageResponse> messagesList =
+                new ArrayList<>();
+        Iterable<Message> messages = messageRepository.findAll();
+        for(Message message : messages) {
+            MessageResponse messageItem = new MessageResponse();
+            messageItem.setName(message.getUser().getName());
+            messageItem.setTime(
+                    formatter.format(message.getSendTime())
+            );
+            messageItem.setText(message.getText());
+            messagesList.add(messageItem);
+        }
+        HashMap<String, List> response = new HashMap<>();
+        response.put("messages", messagesList);
         return response;
     }
 
